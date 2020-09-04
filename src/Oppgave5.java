f11866d6c66a6ee28bdbb117d24b3add7340071d
Hamza-1999 <68568463+Hamza-1999@users.noreply.github.com>
2020-09-04 22:22:06 +0200

    test1

--- src/Oppgave5.java
+++ src/Oppgave5.java
@@ -1,28 +1,35 @@
+import java.util.NoSuchElementException;
+
 public class Oppgave5 {
 
 
     public static void rotasjon(char[] a)
     {
-        int n = 1; // antall ganger man ønsker å rotere arrayet.
-        int j;
+        int n = 1;
 
-        for (int i=0;i<n;i++){ // Roterer den antall ganger N ganger til høyre
-
-            int sisteElement = a[a.length-1]; // lagrer siste element av arrayet, må minus 1 for åfinne det siste element.
-
-            for(j=a.length-1;j>0;j--){ // Ny for loop i en for loop der man shifter elementene,1 og 1
-                a[j] = a[j-1];
+            if (a == null) {
+                System.out.println(0);
             }
 
-            a[0] = (char) sisteElement; // Her settes siste element til å være første. Altså J til å bli A.
+
+if(a.length>0) {
+    for (int i = 0; i < n; i++) {
+
+        int sisteElement = a[a.length - 1];
+
+        for (int j = a.length - 1; j > 0; j--) {
+            a[j] = a[j - 1];
         }
 
+        a[0] = (char) sisteElement;
+    }
 
 
-        for(int i = 0; i< a.length; i++){
-            System.out.print(a[i] + " ");
-        }
+    for (int i = 0; i < a.length; i++) {
+        System.out.print(a[i] + " ");
+    }
 
+}
 
 
     }
@@ -32,7 +39,7 @@ public class Oppgave5 {
 
     public static void main(String args[]) {
 
-        char [] a = {'A','B','C','D','E','F','G','H','I','J'};
+        char [] a = {};
         Oppgave5.rotasjon(a);
 
     }
--- src/Oppgave6.java
+++ src/Oppgave6.java
@@ -3,33 +3,31 @@ import java.util.Arrays;
 public class Oppgave6 {
 
     public static void rotasjon(char[]a, int k){
+        int stElement = a[0];
 
         if (k<0){
-            for (int i = 0; i < k; i++)
+            for (int i = 0; i > k; i--)
             {
-               int temp = a[0];
-
-                for (int j = 0; j < a.length-1; j++)
+                for (int j = 1; j < a.length-1; j++)
                 {
                     a[j] = a[j+1];
                 }
 
-                a[a.length - 1] = (char) temp;
+                a[a.length - 1] = (char) stElement;
 
-        }
+            }
         }
 
 
 
         if(k>0) {
             for (int i = 0; i < k; i++) {
-                int sisteElement = a[a.length - 1];
 
                 for (int j = a.length - 1; j > 0; j--) {
                     a[j] = a[j-1];
                 }
 
-                a[0] = (char) sisteElement;
+                a[0] = (char) stElement;
             }
         }
 
